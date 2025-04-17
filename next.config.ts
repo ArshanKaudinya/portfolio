f4596c396107bb9167a86f1038014e55bbe84e97
Arshan Kaudinya <86201452+ArshanKaudinya@users.noreply.github.com>
2025-04-18 01:37:19 +0530

    First deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,49 @@
 import type { NextConfig } from "next";
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.vercel-insights.com;
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+  img-src * blob: data:;
+  font-src 'self' https://fonts.gstatic.com;
+  connect-src 'self' https://*.supabase.co https://*.vercel-insights.com;
+  frame-src 'none';
+`.replace(/\s{2,}/g, ' ').trim()
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy,
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()',
+  },
+]
+
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
